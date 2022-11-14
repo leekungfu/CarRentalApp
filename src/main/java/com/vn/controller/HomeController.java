f596daf9f949c793e80971f1d22f60aafa02139a
sava <levansang.gthn@gmail.com>
2022-11-14 20:41:10 +0700

    1114 3rd

--- src/main/java/com/vn/controller/HomeController.java
+++ src/main/java/com/vn/controller/HomeController.java
@@ -9,31 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping(value = {"/", "home"})
+    @GetMapping("/home")
     public String getHomePage(Model model) {
         UserDetails detail;
         try {
             detail = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
+            Member member = new Member();
+            member.setFullName(detail.getUsername());
+            model.addAttribute("user", member);
             long countRoleCustomer = detail.getAuthorities().stream().filter(x -> {
                 return x.getAuthority().contains("CUSTOMER");
-            }).count();
-
-            long countRoleOWNER = detail.getAuthorities().stream().filter(x -> {
-                return x.getAuthority().contains("OWNER");
-            }).count();
-            Member member = new Member();
-            member.setFullName("Sava");
-            member.setEmail("levansang.gthn@gmail.com");
-            member.setId(1);
-            member.setRole("CUSTOMER");
-            model.addAttribute("user", member);
-
+            }).count();;
             if (countRoleCustomer > 0) return "home/home_customer";
             return "home/home_car_owner";
-
         } catch (Exception e) {
             return "home/home_guest";
         }
+
     }
 }
